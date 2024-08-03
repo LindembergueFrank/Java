ffc2f40a27357bccb0694217eee588e21f21e92e
LindembergueOliveira <101476903+LindembergueFrank@users.noreply.github.com>
2024-08-03 17:15:25 -0300

    feat: Movendo arquivos para o diretório correto

---
+++ OOP/src/Main.java
@@ -0,0 +1,6 @@
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Diretório de assuntos referentes a Programação orientada a objetos em Java!");
+    }
+}
