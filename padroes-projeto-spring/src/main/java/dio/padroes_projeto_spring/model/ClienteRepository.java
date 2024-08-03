57282010de2962337dddacd5cf81c1fdbac160cd
LindembergueOliveira <101476903+LindembergueFrank@users.noreply.github.com>
2024-08-03 17:11:50 -0300

    remove: Excluindo diretório de padrões de projeto spring

--- padroes-projeto-spring/src/main/java/dio/padroes_projeto_spring/model/ClienteRepository.java
+++
@@ -1,9 +0,0 @@
-package dio.padroes_projeto_spring.model;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ClienteRepository extends CrudRepository<Cliente, Long> {
-
-}
