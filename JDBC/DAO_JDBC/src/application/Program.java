f48a2e0c7d9a95249a45eeff86f31d700d5b0bb1
LindembergueOliveira <101476903+LindembergueFrank@users.noreply.github.com>
2024-08-26 00:57:03 -0300

    :lipstick: feat: Implementando a class Seller

--- JDBC/DAO_JDBC/src/application/Program.java
+++ JDBC/DAO_JDBC/src/application/Program.java
@@ -1,11 +1,17 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 	public static void main(String[] args) {
 		
 		Department department = new Department(1, "Eletronics");
-		System.out.println(department);
+		
+		Seller seller = new Seller(10, "Alex Bob", "alexbobx@gmail.com", new Date(), 2800.0, department);
+		
+		System.out.println(seller);
 	}
 }
