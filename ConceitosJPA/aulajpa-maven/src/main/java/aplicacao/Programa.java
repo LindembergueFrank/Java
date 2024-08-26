5e7c37f70a186881e8aec4302baff0484322dc88
LindembergueOliveira <101476903+LindembergueFrank@users.noreply.github.com>
2024-08-26 19:02:23 -0300

    :lipstick: feat: Adicionando exemplos com JPA

---
+++ ConceitosJPA/aulajpa-maven/src/main/java/aplicacao/Programa.java
@@ -0,0 +1,32 @@
+package aplicacao;
+
+import dominio.Pessoa;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public class Programa {
+
+	public static void main(String[] args) {
+		
+		// Encapsulando conexão com BD e instanciando
+		EntityManagerFactory entitymf = Persistence.createEntityManagerFactory("exemplo-jpa");
+		EntityManager em = entitymf.createEntityManager();
+		
+		em.getTransaction().begin();
+		// Buscando usuario no BD
+		Pessoa pessoa = em.find(Pessoa.class, 2);
+		System.out.println(pessoa);
+		
+		// Removendo usuario (entidade esta monitorada)
+		em.remove(pessoa);
+		
+		em.getTransaction().commit();
+		
+		System.out.println("Feito!");
+		entitymf.close();
+		em.close();
+
+	}
+
+}
